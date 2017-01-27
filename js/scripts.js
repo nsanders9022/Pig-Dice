fdc83a5d80d26ae5ea567f625412f3b20b77bc83
Nicole Sanders <nsanders9022@gmail.com>
2017-01-26 19:32:27 -0800

    include trial code, nothing complete or functioning added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function Player() {
-  this.sumArray = [];
+  this.totalSumArray = [];
+  this.turnArray = [];
+  this.player = 1;
 }
 
 //gets random number between 1 and 6
@@ -7,29 +9,52 @@ Player.prototype.rollDice = function() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
-Player.prototype.rollTotal = function() {
+//tallies up the sum of the dice roll for one turn
+Player.prototype.turnTotal = function() {
   var diceTotal = 0;
 
-  for (var i = 0; i < this.sumArray.length; i++) {
-    diceTotal += parseInt(this.sumArray[i]);
+  for (var i = 0; i < this.turnArray.length; i++) {
+    diceTotal += parseInt(this.turnArray[i]);
   }
   return diceTotal;
 }
 
+// Adds the turn sum to the total sum
+Player.prototype.sumTotal = function() {
+  for (var i = 0; i <this.turnArray.length; i++) {
+    sumTotal += Player.turnTotal();
+  }
+}
 
+//Switches between players
+Player.prototype.playerState = function() {
+  if (this.player === 1) {
+    this.player = 2;
+  } else {
+    this.player = 1;
+  }
+}
 
 
 
 $(document).ready(function() {
 
   var player1 = new Player();
+  var player2 = new Player()
 
   $("#roll").click(function() {
     var thisRoll = player1.rollDice();
-    player1.sumArray.push(thisRoll);
+    player1.turnArray.push(thisRoll);
+
+
+
     $("#roll-number").text(thisRoll);
 
-    $("#player1-total").text(player1.rollTotal());
+    $("#player1-total").text(player1.turnTotal());
+  })
+
+  $("#hold").cllick(function() {
+    player1.player();
   })
 
 
