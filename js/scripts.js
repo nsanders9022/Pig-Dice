4b1a0d6971d0c13d31281055d5e483c5f13b4979
Nicole Sanders <nsanders9022@gmail.com>
2017-01-26 21:35:30 -0800

    alert the user when a 1 is rolled and will reset the turn count to 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 function Player() {
   this.totalSumArray = [];
   this.turnArray = [];
-  this.player = 1;
 }
 
 //gets random number between 1 and 6
@@ -13,30 +12,15 @@ Player.prototype.rollDice = function() {
 Player.prototype.turnTotal = function() {
   var diceTotal = 0;
 
-  if (this.rollDice() !== 1) {
-    for (var i = 0; i < this.turnArray.length; i++) {
-      diceTotal += parseInt(this.turnArray[i]);
-    }
-  } else {
-    turnArray.empty();
+  for (var i = 0; i < this.turnArray.length; i++) {
+    diceTotal += parseInt(this.turnArray[i]);
   }
   return diceTotal;
 }
 
 // Adds the turn sum to the total sum
 Player.prototype.sumTotal = function() {
-  for (var i = 0; i <this.turnArray.length; i++) {
-    sumTotal += Player.turnTotal();
-  }
-}
-
-//Switches between players
-Player.prototype.playerState = function() {
-  if (this.player === 1) {
-    this.player = 2;
-  } else {
-    this.player = 1;
-  }
+  
 }
 
 
@@ -44,20 +28,18 @@ Player.prototype.playerState = function() {
 $(document).ready(function() {
 
   var player1 = new Player();
-  var player2 = new Player()
 
   $("#roll").click(function() {
     var thisRoll = player1.rollDice();
-    player1.turnArray.push(thisRoll);
+    if (thisRoll == 1) {
+      alert("Sorry, you rolled a 1")
+      player1.turnArray = []
+    } else {
+      player1.turnArray.push(thisRoll);
+    }
 
     $("#roll-number").text(thisRoll);
 
-    $("#player1-total").text(player1.turnTotal());
+    $("#player1-turn-total").text(player1.turnTotal());
   })
-
-  $("#hold").cllick(function() {
-    player1.player();
-  })
-
-
 })
